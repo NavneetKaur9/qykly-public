39654290a0460e857b24b59bf468bbcc8060362f
NavneetKaur9 <pannu.navneet9@gmail.com>
2016-07-13 19:06:21 +0530

    address up dated 54.169.236.107 n

--- app/scripts/services/restful.js
+++ app/scripts/services/restful.js
@@ -10,9 +10,9 @@
 angular.module('sbAdminApp').factory('api', ['$rootScope', '$http', '$cookieStore', function($rootScope, $http, $cookieStore) {
 
 	// var parseUrl = 'http://localhost:3000/api2';
-	var parseUrl = 'http://52.66.81.240/api2';
+	// var parseUrl = 'http://52.66.81.240/api2';
 
-	// var parseUrl = 'https://api.qykly.mobi/api2';
+	var parseUrl = 'http://54.169.236.107/api2';
 	// var parseUrl="";
 	var token = $cookieStore.get('c2cCookie'); //set Headers for JWTTOKEN
 	$http.defaults.headers.common.Authorization = 'Bearer ' + token;
