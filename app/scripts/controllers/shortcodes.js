aecae43d77944595394f4b014f78cbcaba718544
NavneetKaur9 <pannu.navneet9@gmail.com>
2016-08-03 23:47:28 +0530

    assigned tab added

--- app/scripts/controllers/shortcodes.js
+++ app/scripts/controllers/shortcodes.js
@@ -17,7 +17,12 @@ angular.module('sbAdminApp').controller('shortcodesCtrl', function($scope, $http
 					$scope.unproc = response.unprocessed;
 					$scope.new = response.newcode;
 					$scope.getShortcode(3);
-				} else {
+				}else if(status === '5'){
+					$scope.reset();
+					$scope.assignedShortcodes=response;
+					$scope.getSms($scope.assignedShortcodes[0], 5);
+				} 
+				else {
 					$scope.proc = response;
 					$scope.getSms($scope.unproc[0], 0);
 					// $scope.alert = false;
@@ -63,11 +68,11 @@ angular.module('sbAdminApp').controller('shortcodesCtrl', function($scope, $http
 			}
 		});
 	};
-	$scope.reset = function(argument) {
+	$scope.reset = function() {
 		$scope.smses = [];
 		$scope.code = "";
 	};
-	$scope.closeAlert = function(argument) {
+	$scope.closeAlert = function() {
 		$scope.alert = false;
 	};
 	$scope.searchCode = '';
@@ -184,5 +189,30 @@ angular.module('sbAdminApp').controller('shortcodesCtrl', function($scope, $http
 			}
 		});
 
+	};
+	$scope.laterUse=function () {
+		$scope.addresses = [];
+		var checkboxes = document.getElementsByName('blacklist');
+		for (var i = 0; i < checkboxes.length; i++) {
+			if (checkboxes[i].checked) {
+				var value = checkboxes[i].value;
+				$scope.addresses.push(value);
+			}
+		}
+		api.put('later-use', false, token, {
+			address: $scope.addresses
+		}, function(err, response) {
+			if (err || response.error) {
+				$scope.alerts = [{
+					msg: response.userMessage || 'Server error! Are you connected to the internet?.',
+					type: 'error'
+				}];
+			} else {
+				$scope.alert = response.message;
+				$scope.smses = [];
+				$scope.getShortcode('0');
+				// $scope.getBlacklisteds();
+			}
+		});
 	};
 });
