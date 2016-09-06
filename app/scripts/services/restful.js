848f00d45c7d3b7030d9e8ef0d227d89d4a647f0
afsar-ali <afsar.co.in@gmail.com>
2016-09-06 12:16:43 +0530

    fixed ui

--- app/scripts/services/restful.js
+++ app/scripts/services/restful.js
@@ -7,8 +7,8 @@
 //
 angular.module('sbAdminApp').factory('api', ['$rootScope', '$http', '$cookieStore', function($rootScope, $http, $cookieStore) {
 
-	 // var parseUrl = 'http://localhost:3000/api2';
-	var parseUrl='/api2';
+	  var parseUrl = 'http://localhost:3000/api2';
+	// var parseUrl='/api2';
 	// var parseUrl = 'http://52.66.81.240/api2'; // staging server address
 	//  var parseUrl = 'http://54.255.175.65/api2'; //  production server address
 	var parseHeaders = {};
